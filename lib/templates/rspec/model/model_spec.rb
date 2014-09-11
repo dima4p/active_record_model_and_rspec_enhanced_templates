eee922f14eb340b9f26c1564022ae080d0b0d1c9
Dmitri Koulikoff (acer) <dima@koulikoff.ru>
2014-09-11 21:30:31 +0400

    Case without factory_girl is fixed

--- lib/templates/rspec/model/model_spec.rb
+++ lib/templates/rspec/model/model_spec.rb
@@ -5,23 +5,26 @@ describe <%= class_name %> do
 
 <% if options[:fixture_replacement] == :factory_girl -%>
   subject { create :<%= singular_name %> }
-
-  it { should be_valid }
 <% else -%>
   before :each do
     @valid_attrs = {
     }
   end
 
-  it "should be valid" do
-    expect(<%= class_name %>.new(@valid_attrs)).to be_valid
-  end
+  subject { <%= class_name %>.create @valid_attrs }
 <% end -%>
+
+  it { should be_valid }
 
   describe :class do
     it 'should respond to :ordered' do
+<% if options[:fixture_replacement] == :factory_girl -%>
       <%= singular_name %>1 = create :<%= singular_name %>
       <%= singular_name %>2 = create :<%= singular_name %>
+<% else -%>
+      <%= singular_name %>1 = <%= class_name %>.create @valid_attrs
+      <%= singular_name %>2 = <%= class_name %>.create @valid_attrs
+<% end -%>
 <% attribute = attributes.detect{|a| a.name == 'position'} || attributes.detect{|a| a.name == 'name'} || attributes.detect{|a| a.name == 'title'} || attributes.first -%>
       expect(<%= class_name %>.ordered).to eq <%= class_name %>.order(:<%=attribute.name %>)
     end
