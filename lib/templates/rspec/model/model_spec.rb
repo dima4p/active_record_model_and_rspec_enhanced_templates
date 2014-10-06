a55bb0537301b834fc62736fb6d80fcde67a9beb
Dmitri Koulikoff (hp470) <dima@koulikoff.ru>
2014-10-06 16:16:44 +0400

    Unneeded varibles assighment is eliminated

--- lib/templates/rspec/model/model_spec.rb
+++ lib/templates/rspec/model/model_spec.rb
@@ -23,11 +23,11 @@ describe <%= class_name %>, type: :model do
   describe :class do
     it 'should respond to :ordered' do
 <% if options[:fixture_replacement] == :factory_girl -%>
-      <%= singular_name %>1 = create :<%= singular_name %>
-      <%= singular_name %>2 = create :<%= singular_name %>
+      create :<%= singular_name %>
+      create :<%= singular_name %>
 <% else -%>
-      <%= singular_name %>1 = <%= class_name %>.create @valid_attrs
-      <%= singular_name %>2 = <%= class_name %>.create @valid_attrs
+      <%= class_name %>.create @valid_attrs
+      <%= class_name %>.create @valid_attrs
 <% end -%>
 <% attribute = attributes.detect{|a| a.name == 'position'} || attributes.detect{|a| a.name == 'name'} || attributes.detect{|a| a.name == 'title'} || attributes.first -%>
       expect(<%= class_name %>.ordered).to eq <%= class_name %>.order(:<%=attribute.name %>)
