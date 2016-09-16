61a554b348fea0494b3821b1ac07b524e3a225f0
Dmitri Koulikoff <dima@koulikoff.ru>
2016-09-16 17:20:03 +0200

    Rails 5 code is added

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -2,17 +2,26 @@
 #
 <% module_namespacing do -%>
 class <%= class_name %> < <%= parent_class_name.classify %>
+<% if attributes.any?(&:reference?) -%>
 
 <% attributes.select(&:reference?).each do |attribute| -%>
-  belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %>
+  belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>
+<% end -%>
+<% end -%>
+<% if attributes.any?(&:token?) -%>
+
+<% attributes.select(&:token?).each do |attribute| -%>
+  has_secure_token<% if attribute.name != "token" %> :<%= attribute.name %><% end %>
+<% end -%>
 <% end -%>
 <% if attributes.any?(&:password_digest?) -%>
+
   has_secure_password
 <% end -%>
 <% if attribute = attributes.detect{|a| a.name == 'position'} || attributes.detect{|a| a.name == 'name'} || attributes.detect{|a| a.name == 'title'} || attributes.first -%>
 
   scope :ordered, -> { order(:<%=attribute.name %>) }
-
 <% end -%>
+
 end
 <% end -%>
