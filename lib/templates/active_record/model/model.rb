5af91012a7a362540e0dcea16f87b77c500446cc
Dmitri Koulikoff <dima@koulikoff.ru>
2017-06-07 13:26:42 +0200

    Legacy support added

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -5,7 +5,7 @@ class <%= class_name %> < <%= parent_class_name.classify %>
 <% if attributes.any?(&:reference?) -%>
 
 <% attributes.select(&:reference?).each do |attribute| -%>
-  belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>
+  belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.respond_to?(:polymorphic?) and attribute.polymorphic? %><%= ', required: true' if  attribute.respond_to?(:required?) and attribute.required? %>
 <% end -%>
 <% end -%>
 <% if attributes.first.respond_to?(:token) and attributes.any?(&:token?) -%>
@@ -14,7 +14,7 @@ class <%= class_name %> < <%= parent_class_name.classify %>
   has_secure_token<% if attribute.name != "token" %> :<%= attribute.name %><% end %>
 <% end -%>
 <% end -%>
-<% if attributes.any?(&:password_digest?) -%>
+<% if (attributes.any?(&:password_digest?) rescue false) -%>
 
   has_secure_password
 <% end -%>
